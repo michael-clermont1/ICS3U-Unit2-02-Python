9c500a6ec8549cae3458cee58426e0553111d1ea
Michael Clermont <michael.clermont@mths.ca>
2022-02-28 19:39:00 +0000

    edited area and perimeter program

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -10,20 +10,20 @@ import math
 
 def main():
     # this function calculates the area and perimeter
-    
-    #input
+
+    # input
     length = int(input("Enter the length of the rectangle (mm): "))
     width = int(input("Enter the width of the rectangle (mm): "))
-    
-    #process
+
+    # process
     area = length * width
-    perimeter = 2 * (length+width)
-    
-    #output
+    perimeter = 2 * (length + width)
+
+    # output
     print("")
     print("Area is {0} mm².".format(area))
     print("Perimeter is {0} mm.".format(perimeter))
-    
+
     print("\nDone.")
 
 
