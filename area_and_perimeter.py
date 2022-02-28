62b0114101eb0d9350e0381df76c628e63329a2f
Michael Clermont <michael.clermont@mths.ca>
2022-02-28 19:35:36 +0000

    added area and perimeter program

---
+++ area_and_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Clermont
+# Created on: Feb 2022
+# This program calculates the area and perimeter of a circle
+#    with radius of 15 mm and user input
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter
+    
+    #input
+    length = int(input("Enter the length of the rectangle (mm): "))
+    width = int(input("Enter the width of the rectangle (mm): "))
+    
+    #process
+    area = length * width
+    perimeter = 2 * (length+width)
+    
+    #output
+    print("")
+    print("Area is {0} mm².".format(area))
+    print("Perimeter is {0} mm.".format(perimeter))
+    
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
